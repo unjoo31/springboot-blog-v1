f786e74342c0cf5cb05a7bf7f67d9131ab9e5477
unjoo31 <unjoo31@gmail.com>
2023-07-25 16:01:15 +0900

    최초커밋

---
+++ src/main/java/shop/mtcoding/blog/controller/HelloController.java
@@ -0,0 +1,12 @@
+package shop.mtcoding.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/")
+    public String test(){
+        return "test";
+    }
+}
